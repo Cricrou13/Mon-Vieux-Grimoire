e47b9f42f25c6c3a4fc025885e5aa66f8367df21
Christophe <delclos.c13@gmail.com>
2026-02-09 11:44:20 +0100

    Architecture + réseau MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,28 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config(); // Pour lire le fichier .env
+require('dotenv').config();
 
+const bookRoutes = require('./routes/books');
+
+const app = express();
+
+// Connexion à la base de données
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
-// Middleware pour gérer les erreurs de CORS
+// Middleware pour les CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Autorise tout le monde
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-// Middleware pour transformer le corps des requêtes en JSON (pour les futurs POST)
+// Middleware pour parser le JSON des requêtes
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.status(200).json({ message: 'Serveur prêt et CORS configurés !' });
-});
+// Routes
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-// On dit à l'application sur quel port elle doit tourner
-
 const port = process.env.PORT || 4000;
-app.set('port' , port);
-
-// On crée le serveur en lui passant notre application app.js
+app.set('port', port);
 
 const server = http.createServer(app);
-
-// On écoute le port défini
 
 server.listen(port, () => {
     console.log(`Le serveur écoute sur le port ${port}`);
